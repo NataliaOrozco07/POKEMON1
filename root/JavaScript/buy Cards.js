23053c75e98b5ebf2d7040e112360b932256a3b0
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-06-16 20:01:23 -0500

    new edit in buyjs

--- root/JavaScript/buy Cards.js
+++ root/JavaScript/buy Cards.js
@@ -7,4 +7,49 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-let url = "https://pokeapi.co/api/v2";
+let url = "https://pokeapi.co/api/v2/pokemon";
+const parentElement = document.querySelector(".grid-container")
+
+const pokePage = async (url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        data.results.forEach(async element => {
+            console.log(element)
+            const card = document.createElement("div");
+            const divName = document.createElement("div");
+            const namePke = document.createElement("p");
+            const icon = document.createElement("i");
+            const img = document.createElement("img");
+            const divLevel = document.createElement("div");
+            const levelPke = document.createElement("p");
+            const bottonBuy = document.createElement("button");
+
+            namePke.textContent = element.name;
+            console.log(element.name)
+
+            const res = await fetch( element.url);
+            const data = await res.json();
+
+            img.src = data.sprites.front_default;
+
+            card.className = 'card'
+            divLevel.className = 'level'
+
+            card.appendChild(divName);
+            divName.appendChild(namePke);
+            divName.appendChild(icon);
+            card.appendChild(img);
+            card.appendChild(divLevel);
+            divLevel.appendChild(levelPke);
+            divLevel.appendChild(bottonBuy);
+            parentElement.appendChild(card);
+        });
+    } catch (error){
+        const errorMsg = document.createElement('p');
+        errorMsg.textContent =`error : ${error.message}`
+        parentElement.appendChild(errorMsg);
+    }
+}
+
+pokePage(url)

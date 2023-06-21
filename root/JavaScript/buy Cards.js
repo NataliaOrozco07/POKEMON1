0c2f246beca35f6e58ac6e3d5b4689896f400048
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-06-21 14:09:01 -0500

    some changes in CSS and JV buy cards

--- root/JavaScript/buy Cards.js
+++ root/JavaScript/buy Cards.js
@@ -7,11 +7,12 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-let url = "https://pokeapi.co/api/v2/pokemon";
+const url = "https://pokeapi.co/api/v2/pokemon";
 const parentElement = document.querySelector(".grid-container");
-const btnMore = document.querySelector(".moreCard");    
 
+const btnMore = document.querySelector(".moreCard");    
 let next;
+btnMore.addEventListener("click", () => pokePage(next));
 
 const pokePage = async (url) => {
     try {
@@ -24,12 +25,12 @@ const pokePage = async (url) => {
         data.results.forEach(async pokemon => {
             const card = document.createElement("div");
             const divName = document.createElement("div");
+            const namePke = document.createElement("p");
             const icon = document.createElement("i");
             const img = document.createElement("img");
             const divLevel = document.createElement("div");
             const levelPke = document.createElement("p");
             const bottonBuy = document.createElement("button");
-            const namePke = document.createElement("p");
 
             namePke.textContent = pokemon.name;
 
@@ -37,18 +38,25 @@ const pokePage = async (url) => {
             const detallesPokemon = await res.json();
             
             img.src = detallesPokemon.sprites.front_default;
+            console.log(detallesPokemon);
             levelPke.textContent = detallesPokemon.base_experience;
 
-            bottonBuy.textContent = 'Buy';
+            const [type1, type2] = detallesPokemon.types.map((pokeType) => pokeType.type.name)
+            console.log(type1);
+            card.setAttribute('pokemonType1', type1);
+            card.setAttribute('pokemonType2', type2);
 
             card.className = 'card';
-            namePke.className ='name';
+            divName.className= 'name'
+            namePke.className ='pName';
             icon.className = "fa-sharp fa-regular fa-heart";
-            img.className = 'card';
+            img.className = 'imgPokemon'
             divLevel.className = 'level';
-            levelPke.className = 'card'
-            bottonBuy.className = 'level';
+            levelPke.className = 'pLevel'
+            bottonBuy.className = 'btnlevel';
+            bottonBuy.textContent = 'Buy';
 
+            parentElement.appendChild(card);
             card.appendChild(divName);
             divName.appendChild(namePke);
             divName.appendChild(icon);
@@ -56,25 +64,13 @@ const pokePage = async (url) => {
             card.appendChild(divLevel);
             divLevel.appendChild(levelPke);
             divLevel.appendChild(bottonBuy);
-            parentElement.appendChild(card);
         });
     } catch (error){
         const errorMsg = document.createElement('p');
         errorMsg.textContent =`error : ${error.message}`
         parentElement.appendChild(errorMsg);
     }
-}
+};
 
 pokePage(url);
-
-
-const nextPage = function(next) {
-    btnMore.addEventListener("click", (next) = pokePage(next));
-}  
-
-
-
-
-
-
 

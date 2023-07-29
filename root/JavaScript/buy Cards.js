17322736b016ffbe919a8a44f5cf62f23831bf35
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-07-29 09:38:48 -0500

    Changes in filter

--- root/JavaScript/buy Cards.js
+++ root/JavaScript/buy Cards.js
@@ -4,7 +4,70 @@ let remainingCards = 0;
 
 const btnMore = document.querySelector(".moreCard");    
 let next;
-btnMore.addEventListener("click", () => pokePage(next));
+btnMore.addEventListener("click", () => showMore(next));
+
+const showMore = async (url) => {
+    try {
+        const res = await fetch(url);
+        const data = await res.json();
+        next = data.next;
+        data.results.forEach(async pokemon => {
+
+            const res  = await fetch(pokemon.url);
+            const detallesPokemon = await res.json();
+
+            const [type1, type2] = detallesPokemon.types.map((pokeType) => pokeType.type.name)
+            const navActive = document.querySelector('.active').textContent.toLowerCase();
+            
+            if(type1===navActive || type2===navActive){
+
+            const card = document.createElement("div");
+            const divName = document.createElement("div");
+            const namePke = document.createElement("p");
+            const icon = document.createElement("i");
+            const img = document.createElement("img");
+            const divLevel = document.createElement("div");
+            const levelPke = document.createElement("p");
+            const bottonBuy = document.createElement("button");
+
+            card.setAttribute('pokemontype1', type1);
+            card.setAttribute('pokemontype2', type2);
+
+            namePke.textContent = pokemon.name;
+            
+            img.src = detallesPokemon.sprites.other["home"].front_default;
+            levelPke.textContent = detallesPokemon.base_experience;
+
+            card.className = 'card';
+            divName.className= 'name'
+            namePke.className ='pName';
+            icon.className = "fa fa fa-heart";
+            img.className = 'imgPokemon'
+            divLevel.className = 'level';
+            levelPke.className = 'pLevel'
+            bottonBuy.className = 'btnlevel';
+            bottonBuy.textContent = 'Buy';
+
+            parentElement.appendChild(card);
+            card.appendChild(divName);
+            divName.appendChild(namePke);
+            divName.appendChild(icon);
+            card.appendChild(img);
+            card.appendChild(divLevel);
+            divLevel.appendChild(levelPke);
+            divLevel.appendChild(bottonBuy);
+
+            remainingCards ++;
+            const carCount = document.querySelector('.count');
+            carCount.textContent = `${remainingCards} cards`;
+        }
+        });
+    } catch (error){
+        const errorMsg = document.createElement('p');
+        errorMsg.textContent =`error : ${error.message}`
+        parentElement.appendChild(errorMsg);
+    }
+};
 
 
 const pokePage = async (url) => {
@@ -64,6 +127,7 @@ const pokePage = async (url) => {
         parentElement.appendChild(errorMsg);
     }
 };
+
 
 const navAll = document.querySelectorAll('.nav-all');
 navAll.forEach((item) => {

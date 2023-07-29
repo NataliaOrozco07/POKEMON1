97816794dc57b742b2eb5aebd5a732c1876aca18
Natalia Orozco López <natalia.orozco07@gmail.com>
2023-07-29 09:54:40 -0500

    logramos el filtro

--- root/JavaScript/buy Cards.js
+++ root/JavaScript/buy Cards.js
@@ -2,7 +2,7 @@ const url = "https://pokeapi.co/api/v2/pokemon";
 const parentElement = document.querySelector(".grid-container");
 let remainingCards = 0;
 
-const btnMore = document.querySelector(".moreCard");    
+const btnMore = document.querySelector(".moreCard");
 let next;
 btnMore.addEventListener("click", () => showMore(next));
 
@@ -13,148 +13,97 @@ const showMore = async (url) => {
         next = data.next;
         data.results.forEach(async pokemon => {
 
-            const res  = await fetch(pokemon.url);
+            const res = await fetch(pokemon.url);
             const detallesPokemon = await res.json();
 
+            //tomando de detalles pokemon los typos del pokemon
             const [type1, type2] = detallesPokemon.types.map((pokeType) => pokeType.type.name)
+           //estamos obteniendo el texto de la barra de navegacion que este seleccionada en el momento
             const navActive = document.querySelector('.active').textContent.toLowerCase();
-            
-            if(type1===navActive || type2===navActive){
-
-            const card = document.createElement("div");
-            const divName = document.createElement("div");
-            const namePke = document.createElement("p");
-            const icon = document.createElement("i");
-            const img = document.createElement("img");
-            const divLevel = document.createElement("div");
-            const levelPke = document.createElement("p");
-            const bottonBuy = document.createElement("button");
-
-            card.setAttribute('pokemontype1', type1);
-            card.setAttribute('pokemontype2', type2);
-
-            namePke.textContent = pokemon.name;
-            
-            img.src = detallesPokemon.sprites.other["home"].front_default;
-            levelPke.textContent = detallesPokemon.base_experience;
-
-            card.className = 'card';
-            divName.className= 'name'
-            namePke.className ='pName';
-            icon.className = "fa fa fa-heart";
-            img.className = 'imgPokemon'
-            divLevel.className = 'level';
-            levelPke.className = 'pLevel'
-            bottonBuy.className = 'btnlevel';
-            bottonBuy.textContent = 'Buy';
-
-            parentElement.appendChild(card);
-            card.appendChild(divName);
-            divName.appendChild(namePke);
-            divName.appendChild(icon);
-            card.appendChild(img);
-            card.appendChild(divLevel);
-            divLevel.appendChild(levelPke);
-            divLevel.appendChild(bottonBuy);
-
-            remainingCards ++;
-            const carCount = document.querySelector('.count');
-            carCount.textContent = `${remainingCards} cards`;
-        }
+          //estoy preguntando si la barra activa es all o sino se muestra segun el tipo
+            if('all' === navActive){
+                drawCard(pokemon,detallesPokemon,type1,type2);
+            } else if (type1 === navActive || type2 === navActive) {
+                drawCard(pokemon,detallesPokemon,type1,type2);
+            }
         });
-    } catch (error){
+    } catch (error) {
         const errorMsg = document.createElement('p');
-        errorMsg.textContent =`error : ${error.message}`
+        errorMsg.textContent = `error : ${error.message}`
         parentElement.appendChild(errorMsg);
     }
 };
 
+function drawCard(pokemon,detallesPokemon,type1, type2) {
+    const card = document.createElement("div");
+    const divName = document.createElement("div");
+    const namePke = document.createElement("p");
+    const icon = document.createElement("i");
+    const img = document.createElement("img");
+    const divLevel = document.createElement("div");
+    const levelPke = document.createElement("p");
+    const bottonBuy = document.createElement("button");
 
-const pokePage = async (url) => {
-    try {
-        const res = await fetch(url);
-        const data = await res.json();
-        next = data.next;
+    card.setAttribute('pokemontype1', type1);
+    card.setAttribute('pokemontype2', type2);
 
-        data.results.forEach(async pokemon => {
-            const card = document.createElement("div");
-            const divName = document.createElement("div");
-            const namePke = document.createElement("p");
-            const icon = document.createElement("i");
-            const img = document.createElement("img");
-            const divLevel = document.createElement("div");
-            const levelPke = document.createElement("p");
-            const bottonBuy = document.createElement("button");
+    namePke.textContent = pokemon.name;
 
-            namePke.textContent = pokemon.name;
+    img.src = detallesPokemon.sprites.other["home"].front_default;
+    levelPke.textContent = detallesPokemon.base_experience;
 
-            const res  = await fetch(pokemon.url);
-            const detallesPokemon = await res.json();
-            
-            img.src = detallesPokemon.sprites.other["home"].front_default;
-            levelPke.textContent = detallesPokemon.base_experience;
+    card.className = 'card';
+    divName.className = 'name'
+    namePke.className = 'pName';
+    icon.className = "fa fa fa-heart";
+    img.className = 'imgPokemon'
+    divLevel.className = 'level';
+    levelPke.className = 'pLevel'
+    bottonBuy.className = 'btnlevel';
+    bottonBuy.textContent = 'Buy';
 
-            const [type1, type2] = detallesPokemon.types.map((pokeType) => pokeType.type.name)
-            card.setAttribute('pokemontype1', type1);
-            card.setAttribute('pokemontype2', type2);
+    parentElement.appendChild(card);
+    card.appendChild(divName);
+    divName.appendChild(namePke);
+    divName.appendChild(icon);
+    card.appendChild(img);
+    card.appendChild(divLevel);
+    divLevel.appendChild(levelPke);
+    divLevel.appendChild(bottonBuy);
 
-            card.className = 'card';
-            divName.className= 'name'
-            namePke.className ='pName';
-            icon.className = "fa fa fa-heart";
-            img.className = 'imgPokemon'
-            divLevel.className = 'level';
-            levelPke.className = 'pLevel'
-            bottonBuy.className = 'btnlevel';
-            bottonBuy.textContent = 'Buy';
+    remainingCards++;
+    const carCount = document.querySelector('.count');
+    carCount.textContent = `${remainingCards} cards`;
+}
 
-            parentElement.appendChild(card);
-            card.appendChild(divName);
-            divName.appendChild(namePke);
-            divName.appendChild(icon);
-            card.appendChild(img);
-            card.appendChild(divLevel);
-            divLevel.appendChild(levelPke);
-            divLevel.appendChild(bottonBuy);
-
-            remainingCards ++;
-            const carCount = document.querySelector('.count');
-            carCount.textContent = `${remainingCards} cards`;
-        });
-    } catch (error){
-        const errorMsg = document.createElement('p');
-        errorMsg.textContent =`error : ${error.message}`
-        parentElement.appendChild(errorMsg);
-    }
-};
 
 
 const navAll = document.querySelectorAll('.nav-all');
 navAll.forEach((item) => {
-    item.addEventListener('click',(event) => {
+    item.addEventListener('click', (event) => {
         const active = document.querySelector('.active');
-        active.classList.replace('active','inactive');
-        event.target.classList.replace('inactive','active');
+        active.classList.replace('active', 'inactive');
+        event.target.classList.replace('inactive', 'active');
         event.preventDefault();
-        const type  = item.textContent.toLowerCase();
+        const type = item.textContent.toLowerCase();
         filterByType(type);
     });
 });
 
 const filterByType = (type) => {
     const cards = document.querySelectorAll('.card');
-    cards.forEach( (card) => {
+    cards.forEach((card) => {
         const cardType1 = card.getAttribute('pokemontype1');
         const cardType2 = card.getAttribute('pokemontype2');
         if (type === 'all' || cardType1 === type || cardType2 === type) {
             card.classList.remove('hide');
-        } else{
+        } else {
             card.classList.add('hide');
         }
     });
 };
 
-pokePage(url);
+showMore(url);
 
 
 
